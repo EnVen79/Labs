c288e12334c87ead8dd98b057ced58ca3cf15449
envenchik <166945760+EnVen79@users.noreply.github.com>
2026-04-21 19:19:22 +0300

    Update GArc.cpp

--- C++/5/GArc.cpp
+++ C++/5/GArc.cpp
@@ -2,7 +2,9 @@
 #include "graphlib.h"
 
 GArc::GArc(): Figure(), centX(0), centY(0), rad(50), strAngle(0), endAngle(180)
-{}
+{
+    draw();
+}
 
 GArc::GArc(float bx, float by,
            float cx, float cy,
@@ -13,14 +15,25 @@ GArc::GArc(float bx, float by,
   centX(cx), centY(cy),
   rad(rad),
   strAngle(strA), endAngle(endA)
-{}
+{
+    draw();
+}
 
 GArc::~GArc()
-{}
+{
+    erase();
+}
 
 void GArc::draw() const
 {
     draw_arc(baseX + centX, baseY + centY,
              rad, strAngle, endAngle,
              colorR, colorG, colorB);
+}
+
+void GArc::erase() const
+{
+    draw_arc(baseX + centX, baseY + centY,
+        rad, strAngle, endAngle,
+        0.0f, 0.0f, 0.0f);
 }
