40e419aa8fd2755f9d018a5ccc826a888cc08912
envenchik <166945760+EnVen79@users.noreply.github.com>
2026-04-28 14:11:02 +0300

    Delete C++/6 directory

--- C++/6/GNumber.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "GObject.h"
-
-class GNumber : public GObject
-{
-public:
-	void oprint() const;
-	~GNumber();
-};
--- C++/6/GObject.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-class GObject
-{
-public:
-	GObject();
-	virtual void oprint() const;
-	~GObject();
-};
